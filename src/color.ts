fc9d4e8752511144e7b1b7a417ccfd8631287f87
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2023-01-21 06:29:59 +0800

    fix(color-validator): improve handling of non-strings

--- src/color.ts
+++ src/color.ts
@@ -2,23 +2,22 @@
 // Copyright (c) Tailwind Labs, Inc. (https://tailwindcss.com/)
 // and used under the terms of the MIT license
 
-import namedColors from "color-name"
+import Colors from "color-name"
 import {
   TailwindColorGroup,
   TailwindThemeColors,
 } from "tailwindcss/tailwind-config"
-import { Color, Maybe } from "./types"
+import invariant from "tiny-invariant"
+import { Color } from "./types"
 
 const HEX = /^#([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i
 const SHORT_HEX = /^#([a-f\d])([a-f\d])([a-f\d])([a-f\d])?$/i
 const VALUE = `(?:\\d+|\\d*\\.\\d+)%?`
 const SEP = `(?:\\s*,\\s*|\\s+)`
 const ALPHA_SEP = `\\s*[,/]\\s*`
+
 const RGB = new RegExp(
   `^rgba?\\(\\s*(${VALUE})${SEP}(${VALUE})${SEP}(${VALUE})(?:${ALPHA_SEP}(${VALUE}))?\\s*\\)$`
-)
-const HSL = new RegExp(
-  `^hsla?\\(\\s*((?:${VALUE})(?:deg|rad|grad|turn)?)${SEP}(${VALUE})${SEP}(${VALUE})(?:${ALPHA_SEP}(${VALUE}))?\\s*\\)$`
 )
 
 export const flattenColorPalette = (
@@ -35,38 +34,17 @@ export const flattenColorPalette = (
     )
   )
 
-export function parseColor(value: string): Maybe<Color> {
-  if (!value) return null
-
+export function parseColor(value: string): Color {
   value = value.trim()
+
   if (value === "transparent") {
     return { mode: "rgb", color: ["0", "0", "0"], alpha: "0" }
   }
 
-  if (value in namedColors) {
+  if (value in Colors) {
     return {
       mode: "rgb",
-      color: namedColors[value as keyof typeof namedColors].map((v) =>
-        v.toString()
-      ),
-    }
-  }
-
-  const hex = value
-    .replace(SHORT_HEX, (_, r, g, b, a) =>
-      ["#", r, r, g, g, b, b, a ? a + a : ""].join("")
-    )
-    .match(HEX)
-
-  if (hex) {
-    return {
-      mode: "rgb",
-      color: [
-        parseInt(hex[1], 16),
-        parseInt(hex[2], 16),
-        parseInt(hex[3], 16),
-      ].map((v) => v.toString()),
-      alpha: hex[4] ? (parseInt(hex[4], 16) / 255).toString() : undefined,
+      color: Colors[value as keyof typeof Colors].map((v) => v.toString()),
     }
   }
 
@@ -80,20 +58,27 @@ export function parseColor(value: string): Maybe<Color> {
     }
   }
 
-  const hslMatch = value.match(HSL)
+  // We already filter out and validate before even trying
+  // to parse, so this should now always match a hex
+  const hex = value
+    .replace(SHORT_HEX, (_, r, g, b, a) =>
+      ["#", r, r, g, g, b, b, a ? a + a : ""].join("")
+    )
+    .match(HEX)
 
-  if (hslMatch) {
-    return {
-      mode: "hsl",
-      color: [hslMatch[1], hslMatch[2], hslMatch[3]].map((v) => v.toString()),
-      alpha: hslMatch[4]?.toString?.(),
-    }
+  invariant(hex, `Invalid value: ${value}`)
+
+  return {
+    mode: "rgb",
+    color: [
+      parseInt(hex[1], 16),
+      parseInt(hex[2], 16),
+      parseInt(hex[3], 16),
+    ].map((v) => v.toString()),
+    alpha: hex[4] ? (parseInt(hex[4], 16) / 255).toString() : undefined,
   }
-
-  return null
 }
 
-export function formatColor({ mode, color, alpha }: Color) {
-  if (!color) return null
-  return `${mode}(${color?.join(" ")}${alpha ? ` / ${alpha}` : ""})`
+export function formatColor({ mode, color, alpha }: Color): string {
+  return `${mode}(${color.join(" ")}${alpha ? ` / ${alpha}` : ""})`
 }
