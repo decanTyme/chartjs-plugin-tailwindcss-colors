5914ad5aaeba8f1f6d0d4dc8644ce478ecdbc4ce
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2023-02-19 18:44:16 +0800

    on second thought, attach even when native esm is support

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import twColorsPlugin from "./plugin"
 
-// https://gist.github.com/ebidel/3201b36f59f26525eb606663f7b487d0#gistcomment-4102094
-function hasModuleSupport() {
-  if ("supports" in HTMLScriptElement) {
-    return HTMLScriptElement.supports("module")
-  }
-  return "noModule" in document.createElement("script")
-}
-
 // Since Parcel v2 doesn't have proper support for global expose yet,
 // manually attach the plugin to the `window` object as a workaround
 // @see https://github.com/parcel-bundler/parcel/discussions/5583
 // @see https://github.com/parcel-bundler/parcel/issues/7312
 // @see https://github.com/parcel-bundler/parcel/pull/7240
 // @see https://v1.parceljs.org/cli.html#expose-modules-as-umd
-if (typeof window !== "undefined" && !hasModuleSupport()) {
+if (typeof window !== "undefined") {
   // @ts-ignore
   window.twColorsPlugin = twColorsPlugin
 }
