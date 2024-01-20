558dbf5d3bdb68c6def63767a2e719f95b6c123f
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2024-01-20 16:46:16 +0800

    migrate `ts-jest` and jest config

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { JestConfigWithTsJest } from "ts-jest"
+import { defaults } from "ts-jest/presets"
+
+const config: JestConfigWithTsJest = {
+  runner: "@kayahr/jest-electron-runner",
+  testEnvironment: "@kayahr/jest-electron-runner/environment",
+  testMatch: ["*/**/__tests__/*.(spec|test).ts"],
+  testPathIgnorePatterns: ["dist"],
+  coveragePathIgnorePatterns: ["test"],
+
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      { ...defaults, tsconfig: "./src/__tests__/tsconfig.test.json" },
+    ],
+  },
+}
+
+export default config
